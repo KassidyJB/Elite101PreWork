98cf0e1e63f94b6552f6cb044f10d520d504bd8b
KassidyJB <jeanbartkassidy51@gmail.com>
2024-11-13 03:28:24 +0000

    changes on all functions, Testing it by running. Keep getting KeyboardInterrupt though.

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import random
 
-print("--------------------------")
-name = input("What is your name? \n>")
+program_loop = True
+
+name = input(f"What is your name? \n>")
 
 print(f'\nWelcome to JB Bank {name}!\n')
 
@@ -9,10 +10,8 @@ age = input(f'So {name}, how old are you?? \n>')
 
 print(f'\nLord!! {age}!? That is so cool!!\n')
 
-program_loop = True
-
 def display_menu():
-  print(f' Anyways, it seems like you need help {name}.\n')
+  print(f' \nAnyways, it seems like you need help {name}.\n')
   print("1. Sign in") #Can use the sign in function to create saving and checking account 
   print("2. Log In") #Can use Log in to transfer, deposit, or withdraw money
   print("3. Learn More") #Gives user any additional details needed or anything they could learn
@@ -22,9 +21,8 @@ display_menu()
   
 def selection():
   in_use = True
-  print("--------------------")
-  choice = int(input('''
-  Enter your choice so I can properly assist. \n>'''))
+  print("------------------------------------------")
+  choice = int(input('Enter your choice so I can properly assist. \n>'))
   if choice == 1:
     choice_1()
   elif choice == 2:
@@ -32,59 +30,61 @@ def selection():
   elif choice == 3:
     choice_3()
   elif choice == 4:
-    print(f"Thank you for using JB BANK. See you next time, {name}!\n")
-    print("------------------------")
+    print(f"\nThank you for using JB BANK. See you next time, {name}!\n")
+    print("---------------------------------------")
     in_use = False
 
-selection()
-
 def choice_1():
-  user_name = input(f"{name}, Please enter a username for your new account below. \n")
-  print(f"Hello {user_name}!\n")
-  cs_ask = input(f"{name}, would you like to create a savings or checking account?\n").capitalize()
+  print("------------------------------------------------")
+  user_name = input(f"\n{name}, Please enter a username for your new account below. \n>")
+  print(f"\nHello {user_name}!\n")
+  cs_ask = input(f"\n{name}, would you like to create a savings or checking account? \n>").capitalize()
   
-  if cs_ask == "Savings":#So now, because I didn't have s or c capital, input HAS to be lowercase---->(fized using .capitalize())
-    acc_select = input(f"Alright {user_name}, you selected Savings. Would you like to create a joint account or single account?\n").capitalize()
+  if cs_ask == "Savings":
+    acc_select = input(f"\nAlright {user_name}, you selected Savings. Would you like to create a joint account or single account? \n>").capitalize()
     
     if acc_select == "Single account":
+        user_intel_1 = []
+        user_info_1 = input(f"\nEnter your address below, {user_name} \n>").capitalize()
     
     if acc_select == "Joint account":
+      user_intel_2 = []
+      user_info_2 = input(f"\nEnter you address below, {user_name} \n>").capitalize()
+      user_intel_2.append(user_info_2)
   
-  if cs_ask == "Checking account": 
-    user_intel = []
-    user_info = input(f"Enter your adress below, {user_name}\n").capitalize()
-pass
-#choice_1()
+  if acc_select == "Checking account": 
+    user_intel_3 = []
+    user_info = input(f"\nEnter your adress below, {user_name} \n>").capitalize()
+    user_intel_3.append(user_info)
+
+
+
 
 def choice_2():
   print("------------------------------------------------")
-  log_name = input(f"Enter your username below, {name}")
-  log_ask = input(f"{log_name}, would you like to transfer, deposit, or withdraw money?\n").capitalize()
+  log_name = input(f"\nEnter your username below, {name} \n>")
+  log_ask = input(f"\n{log_name}, would you like to transfer, deposit, or withdraw money? \n>").capitalize()
   
   if log_ask == "Transfer":
-    tran = float(input("How much money would you like to transfer?"))
+    tran = float(input(f"\nHow much money would you like to transfer? \n>"))
   
   if log_ask == "Deposit":
-    dep = float(input("How much money would you like to deposit?"))
+    dep = float(input(f"\nHow much money would you like to deposit? \n>"))
   
   if log_ask == "Withdraw":
-    wdraw = float(input("How much money would you like to withdraw?"))
+    wdraw = float(input(f"\nHow much money would you like to withdraw? \n>"))
 
 
-
-pass
 
 def choice_3():
-  print("-------------------------------------")
-  print(f"Here's a few things to know: \n")
-  print('''Our ATM machines are located in each borough of NY, 7 in NJ, 
-        4 in New Hampshire, and 3 in Pennsylvania''')
-  print('''We at JB Bank prioritize your safety and will be the ones keeping your money in your pockets. 
-        With that being said, you can earn up to $100 in your account by the end of each year you're with us. 
-        Happy Banking!''')
+  print("--------------------------------------------------------------")
+  print(f"\nHere's a few things to know: \n")
+  print(f'''\nOur ATM machines are located in each borough of NY, 7 in NJ, 
+  4 in New Hampshire, and 3 in Pennsylvania \n''')
+  print(f'''\nWe at JB Bank prioritize your safety and will be the ones keeping your money in your pockets. 
+  With that being said, you can earn up to $100 in your account by the end of each year you're with us. 
+  Happy Banking!''')
 
-
-pass
 
 while program_loop:
   display_menu()
